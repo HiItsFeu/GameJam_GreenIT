308b7657b99e6cd8fa81662f76981b04c841461b
Tsukini <167094972+TsukiNi22@users.noreply.github.com>
2025-12-13 13:54:26 +0100

    feat (vxf death): add a respawn timer + death effect

--- Assets/Scripts/Game Behavior/PlayerRespawnDeath.cs
+++ Assets/Scripts/Game Behavior/PlayerRespawnDeath.cs
@@ -1,20 +1,39 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerRespawnDeath : MonoBehaviour
 {
     public Vector3 respawnPoint;
+    public ParticleSystem deathPS;
+    Rigidbody2D playerRb;
+    SpriteRenderer spriteRenderer;
 
-    public void Respawn()
+    private void Awake()
     {
-        Debug.Log("Player has respawn!");
-        // effect
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        respawnPoint = transform.position;
+    }
+
+    IEnumerator Respawn(float duration)
+    {
+        playerRb.simulated = false;
+        spriteRenderer.enabled = false;
+        yield return new WaitForSeconds(duration);
         transform.position = respawnPoint;
+        playerRb.simulated = true;
+        spriteRenderer.enabled = true;
     }
 
     public void Kill()
     {
-        Debug.Log("Player has died!");
-        // effect timer and other thing
-        Respawn();
+        deathPS.transform.position = transform.position;
+        deathPS.Emit(20);
+        StartCoroutine(Respawn(1f));
     }
 }
